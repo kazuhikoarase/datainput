16d6ad29794c8f802c25b9719933556d93a16c08
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2018-04-12 18:46:13 +0900

    commit

--- demo/assets/index.js
+++ demo/assets/index.js
@@ -3,24 +3,24 @@ window.addEventListener('load', function() {
 
   var $d = datainput;
 
-  var dataIn1 = $d.createInput({ format : 'XXX-XXXX' }).
+  var dataIn1 = $d.createInput({ format : '00000' }).
     on('valuechange', function(event, detail) {
       console.log(event, detail);
     });
-  dataIn1.setValues(['123', '4567'])
+  dataIn1.setValues(['12345'])
 
-  var dataIn2 = $d.createInput({ format : 'XXX/XX/XX', textAlign : 'right' });
+  var dataIn2 = $d.createInput({ format : '0000/00/00', textAlign : 'right' });
 
   var dataIn3 = $d.createInput({
-      format : 'XXXX年XX月XX日',
+      format : '0000年00月00日',
       getNumCharsInMonospace : function(c) {
         return '年月日'.indexOf(c) != -1? 2 : 1;
       },
       textAlign : 'right'
     });
 
-  var dataIn4 = $d.createInput({ format : 'XXXX-XXXX-XXXX-XXXX' });
-  var dataIn5 = $d.createInput({ format : 'XX-X-XXX-XXXX' });
+  var dataIn4 = $d.createInput({ format : '0000-0000-0000-0000' });
+  var dataIn5 = $d.createInput({ format : 'XX-X-XX-XXX', sectionRe : '\\w+' });
   dataIn5.on('blur', function(event, detail) {
     console.log(event, detail);
   });
